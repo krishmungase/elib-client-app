360b5862c53753a1753c7729935bc8c195b423a8
krishmungase <mungasekrishna8@gmail.com>
2024-08-05 18:17:25 +0530

    Footer added

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -4,7 +4,7 @@ import BookCard from './BookCard'
 
 const BookList = ({books} : {books : BOOK[]}) => {
   return (
-    <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto rounded-[5px]'>
+    <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto rounded-lg mb-6'>
       {
         books.map((book) => (
           <BookCard key={book._id} book={book} />
